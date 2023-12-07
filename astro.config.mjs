68b842f38fb221a2cc8aceba319900ff24e1cad9
Leo Merkel <leo@leomerkel.de>
2023-12-07 11:24:29 +0100

    RSS Feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  "experimental.contentCollectionCache": true
+  "experimental.contentCollectionCache": true,
+  site: "https://build-a-blog-work.netlify.app/"
 });
